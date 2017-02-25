8e185a6f1233eaa97af9cf3b46919bd7b4a57a6a
Jonás Perusquía Morales <jonaspm_99@outlook.com>
2017-02-24 19:28:04 -0700

    Initial commit. Full project

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hola mundo');
-});
+    
+    res.send('Hola!')
+    res.end()
+})
 
-app.listen(3000);
+app.get('/:exp', (req, res) => {
+  try {
+    let result = eval(req.params.exp)
+    res.send(result.toString())
+  } catch(ex) {
+    res.send(ex.toString())
+  }
+  res.end()
+})
+
+var port = (process.env.PORT || '3000')
+
+app.listen(port, () => {
+  console.log('App listening into port:', port)
+})
